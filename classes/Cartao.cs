ae245e8e09d17c804f72b5ada00f650f99b3de0a
Enzzo Lima Alves <enzzolimaalves2013@gmail.com>
2020-12-16 17:47:27 -0300

    Finalizado com desafio

--- classes/Cartao.cs
+++ classes/Cartao.cs
@@ -6,14 +6,9 @@ namespace POO_LISTADEOBJETOS_EX1.classes
     public class Cartao
     {
         public string Titular{ get;set;}
-
         public int Numero{get;set;}
-
         public string Bandeira{get;set;}
-
         public string Vencimento{get;set;}
-
-
         private int cvv;
         public int Cvv{get{return cvv;}set{cvv = value;}}
 
@@ -21,7 +16,6 @@ namespace POO_LISTADEOBJETOS_EX1.classes
         public Cartao(){
 
         }
-
         public Cartao(string _titular , int _numero , string _bandeira , string _vencimento , int _cvv){
             Titular = _titular;
             Numero = _numero;
